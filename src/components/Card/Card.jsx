03cb82cd251845fec9f6320feee32d38daf636df
Theo-Ross <TheoRoss99@icloud.com>
2022-12-07 17:13:25 +0000

    data map complete

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,16 @@
-import "./Card.scss"
+import "./Card.scss";
+import data from "../../data/data";
 
+const Card = ({ name, image_url, description }) => {
+  return (
+    <div className="card">
+      <img src={image_url} alt="beer image" className="card__image" />
+      <h2 className="card__h2">This is the name of the beer : {name}</h2>
+      <p className="card__info">
+        This is the information for the beer : {description}
+      </p>
+    </div>
+  );
+};
 
-
-const Card = () => {
-    return (
-      <div className="card">
-        
-
-      </div>
-    );
-  };
-  
-  export default Card;
+export default Card;
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,12 +1,17 @@
 import "./CardList.scss";
 import Card from "../Card/Card";
+import data from "../../data/data";
 
-const CardList = () => {
-  return (
-    <div className="cardlist">
-      <Card />
-    </div>
-  );
+const CardList = (props) => {
+  return data.map((index) => {
+    return (
+      <Card
+        name={index.name}
+        image_url={index.image_url}
+        info={index.description}
+      />
+    );
+  });
 };
 
 export default CardList;
