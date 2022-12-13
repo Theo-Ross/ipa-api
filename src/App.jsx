cf4f7b8324151d409960a1f7d210d939f348d951
Theo-Ross <TheoRoss99@icloud.com>
2022-12-13 14:36:26 +0000

    buttons able to click and unclick

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [abvButton, setAbv] = useState(0);
-  const [classicButton, setClassic] = useState(2022);
+  const [classicButton, setClassic] = useState(2023);
   const [filteredData, setFilteredData] = useState([]);
   const [phButton, setPhButton] = useState(0);
 
@@ -20,17 +20,38 @@ const App = () => {
   };
 
   const handleOnChange = (event) => {
-    if (event.target.innerText === "High ABV over 6.0%") {
+    if (event.target.innerText === "High ABV over 6.0%" && abvButton === 0) {
       return setAbv(6);
-    } else if (event.target.innerText === "Acidic ph over 4") {
+    } else if (
+      event.target.innerText === "High ABV over 6.0%" &&
+      abvButton === 6
+    ) {
+      return setAbv(0);
+    } else if (
+      event.target.innerText === "Acidic ph over 4" &&
+      phButton === 0
+    ) {
       return setPhButton(4);
-    } else if (event.target.innerText === "Classic Range") {
+    } else if (
+      event.target.innerText === "Acidic ph over 4" &&
+      phButton === 4
+    ) {
+      return setPhButton(0);
+    } else if (
+      event.target.innerText === "Classic Range" &&
+      classicButton === 2023
+    ) {
       return setClassic(2010);
+    } else if (
+      event.target.innerText === "Classic Range" &&
+      classicButton === 2010
+    ) {
+      return setClassic(2023);
     }
   };
 
   const getBeers = async (abvResult, classicResult) => {
-    const url = `https://api.punkapi.com/v2/beers?${searchTerm}abv_gt=${abvResult}&brewed_before=11-${classicResult}`;
+    const url = `https://api.punkapi.com/v2/beers?${searchTerm}abv_gt=${abvResult}&brewed_before=11-${classicResult}&per_page=80`;
     const result = await fetch(url);
     const beerData = await result.json();
     if (phButton == 4) {
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,13 @@ import "./Card.scss";
 import data from "../../data/data";
 
 const Card = ({ name, image_url, description, abv, ph }) => {
+  const textShortener = description.substring(0, 100) + " ...";
+
   return (
     <div className="card">
       <img src={image_url} alt="beer image" className="card__image" />
       <h2 className="card__h2">{name}</h2>
-      <p className="card__info"> {description}</p>
+      <p className="card__info"> {textShortener}</p>
       <p>abv: {abv}</p>
       <p>ph: {ph}</p>
     </div>
